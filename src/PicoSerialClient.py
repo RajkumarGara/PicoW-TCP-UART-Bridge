74d5cd8a3badadd93d8be927d76f7258644579c9
RajkumarGara <rajkumar.gara3@gmail.com>
2024-02-16 13:02:44 -0500

    updated led status, tcp data packet size

--- src/PicoSerialClient.py
+++ src/PicoSerialClient.py
@@ -4,12 +4,13 @@ import time
 from machine import UART, Pin
 
 # Network credentials
-WIFI_SSID = 'your_wifi_ssid'
-WIFI_PASSWORD = 'your_wifi_password'
+WIFI_SSID = 'mieweb-corp'
+WIFI_PASSWORD = 'mieweb internet!'
 
 # Server details
-IP_ADDRESS = 'your_device_ip_address'
+IP_ADDRESS = '10.3.136.129'
 TCP_PORT = 50000
+PICO_ID = 1
 
 # Initialize UART and LED
 uart1 = UART(1, 19200)  # Adjust UART settings as per your configuration
@@ -17,7 +18,9 @@ uart1.init(19200, bits=8, parity=None, stop=1, tx=4, rx=5)
 led = Pin("LED", Pin.OUT)
 
 def blink_led():
-    led.toggle()
+    led.off()
+    time.sleep(0.1)
+    led.on()
 
 # Connect to Wi-Fi
 wlan = network.WLAN(network.STA_IF)
@@ -26,64 +29,62 @@ wlan.connect(WIFI_SSID, WIFI_PASSWORD)
 
 while not wlan.isconnected():
     blink_led()
-    time.sleep(0.1)
 
 led.off()
 print("Connected to WiFi")
 
-# Establish a new TCP connection with retry mechanism
+# Establish a new TCP connection
 def create_tcp_connection():
     while True:
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((IP_ADDRESS, TCP_PORT))
-            print("Connected to TCP server")
+            led.on()
             return sock
+        
         except Exception as e:
             print(f"Failed to connect to TCP server: {e}")
             print("Retrying in 5 seconds...")
             time.sleep(5)
 
-# Send 'hello' packet to initiate named pipe creation
+# Send 'hello' packet to initiate file creation
 def send_hello_packet(sock):
-    hello_message = 'pico_1'
+    hello_message = f'pico_{PICO_ID}'
     sock.send(hello_message.encode())
 
 # Create initial TCP socket and connect
 s = create_tcp_connection()
+print("Connected to TCP server")
 
 # Send hello packet and wait for confirmation
 send_hello_packet(s)
 
-led.on()
-
 try:
     while True:
-        # Check for incoming UART data first
+        # Check for incoming UART data
         if uart1.any():
             rxed = uart1.read().decode('utf-8').rstrip()
             s.send(rxed.encode())
             blink_led()
-            # print("Serial device: ", rxed)
+            #print("Serial: ", rxed)
 
         # Non-blocking check for TCP data
         s.setblocking(False)
         try:
-            data = s.recv(1024)
+            data = s.recv(64)
             if data == b'':  # TCP connection closed
+                s.close()        
+                led.off()
                 print("TCP connection closed by server. Reconnecting...")
-                s.close()
                 s = create_tcp_connection()
-                # Send hello packet and wait for confirmation
-                send_hello_packet(s)
+                send_hello_packet(s) # Send hello packet and wait for confirmation
                 print("Reconnected to server.")
                 continue
 
-            if data:
+            if data:	# Data received from TCP Server
                 cmd = data.decode()
-                uart1.write(cmd)
                 blink_led()
-                # print("TCP: ", cmd)
+                #print("TCP: ", cmd)
 
         except Exception as e:
             pass  # No data received, normal for non-blocking call
@@ -93,6 +94,8 @@ try:
 
 except Exception as e:
     print("An error occurred:", e)
+    
 finally:
     s.close()
+    led.off()
     print("TCP socket closed")
