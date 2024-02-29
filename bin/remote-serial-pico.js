5e71fe115fb90e62bcf3da779dfbb54d4fa5f6ed
RajkumarGara <rajkumar.gara3@gmail.com>
2024-02-29 16:42:35 -0500

    updated udev, remote-serial-pico.js for proper project setup using npm

--- bin/remote-serial-pico.js
+++ bin/remote-serial-pico.js
@@ -1,28 +1,32 @@
+#!/usr/bin/env node
+
 const { execSync } = require('child_process');
 
-const installRshellCommands = `
-sudo mkdir -m 777 /home/project && cd /home/project
-sudo apt update && sudo apt install -y python3-venv python3-pip
-python3 -m venv myenv && source myenv/bin/activate
-pip install rshell
-`;
+const installRshellCommands = [
+  'if [ ! -d "/home/project" ]; then sudo mkdir -m 777 /home/project; fi',
+  'cd /home/project',
+  'sudo apt update && sudo apt install -y python3-venv python3-pip',
+  'python3 -m venv myenv',
+  '/home/project/myenv/bin/pip install rshell'
+].join(' && ');
 
 const setupProjectCommands = `
-cd /home/project
-git clone https://github.com/RajkumarGara/remote-serial-pico
-cd /home/project/remote-serial-pico/src
-sudo cp 99-pico.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
+cd /home/project && 
+if [ ! -d "remote-serial-pico" ] || [ -z "$(ls -A remote-serial-pico)" ]; then 
+  git clone https://github.com/RajkumarGara/remote-serial-pico; 
+else 
+  echo "Directory remote-serial-pico already exists and is not empty. Skipping clone."; 
+fi && 
+cd /home/project/remote-serial-pico/src && 
+sudo cp 99-pico.rules /etc/udev/rules.d/ && 
+sudo udevadm control --reload-rules && 
 sudo udevadm trigger
 `;
 
+
 function runCommands(commands) {
-  commands.split('\n').forEach((command) => {
-    if (command.trim()) {
-      console.log(`Executing: ${command}`);
-      execSync(command, { stdio: 'inherit', shell: true });
-    }
-  });
+  console.log(`Executing: ${commands}`);
+  execSync(commands, { stdio: 'inherit', shell: true });
 }
 
 console.log("Installing rshell and other dependencies...");
